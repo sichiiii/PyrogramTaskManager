21d62a0293a720997593d8a8f29ff3cc7a3509c6
sichiiii <kirillfisyukovich@gmail.com>
2022-04-27 10:14:52 +0300

    bot -> api

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import uuid
-import telebot
 import asyncio
 import aiofiles
 import databases
@@ -18,7 +17,6 @@ database = databases.Database(DATABASE_URL)
 
 logger = app_logger.get_logger(__name__)
 tgSendApp = FastAPI()
-bot = telebot.TeleBot('5367760046:AAFhNxfSjYSaLccGBCbzAZ5q-JHFnaEIjsM')  # TODO: bot -> api
 
 
 async def check_emotions():
@@ -35,14 +33,12 @@ async def check_emotions():
             if message.from_user.id == 1003945710:
                 if message.reactions:
                     emoji = message.reactions[0].emoji
-                    print(emoji)
                     if emoji == '🔥':
                         await message.delete()
                     elif emoji == '👍':
                         message_end = message.text[-14:]
                         if message_end != " - В обработке":
                             await message.edit_text(message.text + " - В обработке")
-        await app.stop()
     except Exception as ex:
         print(str(ex))
 
@@ -64,7 +60,7 @@ def repeat_every(*, wait_first: bool = False):
                             await run_in_threadpool(func)
                     except Exception as e:
                         logger.error(str(e))
-                    await asyncio.sleep(1)
+                    await asyncio.sleep(5)
             asyncio.create_task(loop())
         return wrapped
     return decorator
@@ -79,17 +75,24 @@ async def remove_expired_tokens_task():
 @tgSendApp.post('/')
 async def chat(chat_id: int = Form(...), text: Optional[str] = Form(None), file: Optional[UploadFile] = File(None)):
     try:
+        chat_id = -716352016
+        app = Client(
+            "cyberjabka",
+            api_id=2823137,
+            api_hash='df822805777e34b345af4e53bd07c246'
+        )
+        await app.start()
         if file:
             file.filename = f"{uuid.uuid4()}.jpg"
             picture_path = f'{os.getcwd()}/storage/{file.filename}'
             async with aiofiles.open(picture_path, 'wb') as out_file:
                 contents = await file.read()
                 await out_file.write(contents)
-            bot.send_photo(chat_id, photo=open(picture_path, 'rb'))
+            await app.send_photo(chat_id, photo=open(picture_path, 'rb'))
             os.remove(picture_path)
             return {"status": "ok", "filename": file.filename}
         if text:
-            bot.send_message(-716352016, str(text))
+            await app.send_message(chat_id, str(text))
             return {"status": "ok", "text": text}
         raise Exception('Wrong data (specify picture or text)')
     except Exception as ex:

9f16b14b6c909cfbb8868c64a37668ff33944b6e
jabka <sichiiii@github.com>
2022-06-13 22:56:15 +0300

    fix float hours

--- app/time_message_parser.py
+++ app/time_message_parser.py
@@ -8,24 +8,32 @@ class TimeMesParser:
     def __init__(self):
         self.logger = get_logger(__name__)
 
+    @staticmethod
+    def check_float_str(message_text: str):
+        try:
+            float(message_text)
+            return True
+        except ValueError:
+            return False
+
     def parse_time_message(self, message_text: str):
         try:
-            if re.match(r'^(2[0-3]|[01]?\d):([0-5]?\d)$', message_text): # TODO: make regex grouping
+            if re.match(r'^(2[0-3]|[01]?\d):([0-5]?\d)$', message_text):  # TODO: make regex grouping
                 now = datetime.now()
                 current_time = datetime.strptime(now.strftime('%H:%M:%S'), '%H:%M:%S').time()
                 message_time_obj = datetime.strptime(message_text, '%H:%M').time()
                 current_hours, current_minutes = int(str(current_time)[:-6]), int(str(current_time)[3:-3])
-                future_hours,  future_minutes = int(str(message_time_obj)[:-6]), int(str(message_time_obj)[3:-3])
+                future_hours, future_minutes = int(str(message_time_obj)[:-6]), int(str(message_time_obj)[3:-3])
                 if message_time_obj > current_time:
                     return (future_hours - current_hours) * 3600 + (future_minutes - current_minutes) * 60
                 else:
                     return (24 - current_hours + future_hours) * 3600 + (60 - current_minutes + future_minutes) * 60
-            else:
-                try:
-                    buffer = float(message_text) * 3600
-                    return float(message_text)
-                except ValueError:
-                    return None
         except Exception as ex:
-            self.logger.error(str(ex))
-            return None
+            if self.check_float_str(message_text):
+                return float(message_text)
+            else:
+                return None
+
+if __name__ == '__main__':
+    t = TimeMesParser()
+    print(t.parse_time_message(4))
